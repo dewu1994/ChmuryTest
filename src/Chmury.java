42dd63e113961a69c01ec6ede45973fcf43f61e2
Kubaru1130 <jakubsmieszek11@gmail.com>
2019-03-20 20:49:19 +0100

    testt

--- src/Chmury.java
+++ src/Chmury.java
@@ -4,7 +4,7 @@ public class Chmury {
 	public static void main(String[] args) {
 		String version = System.getProperty("java.version");
 		System.out.println(version);
-
+		System.out.println("teeest");
 	}
 
 }
